509fe0917c143697bc50f2241ad583ba6fa0b308
David <debukali@gmail.com>
2024-06-19 06:28:08 +0300

    Protect api routes using middleware and token checks

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
+import { protect } from './modules/auth';
 
 const app = express();
+
+const customLogger = (message) => (req, res, next) => {
+
+}
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use((req, res, next) => {
+    req.shhhh_secret = "dodgy";
+    next()
+});
 
 app.get('/', (req, res) => {
     console.log('Hello from express');
@@ -9,5 +27,7 @@ app.get('/', (req, res) => {
         message: 'hello'
     })
 });
+
+app.use('/api', protect, router);
 
 export default app;
